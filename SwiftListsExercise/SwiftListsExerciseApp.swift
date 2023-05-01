8059a621cb06e93e4680e345282411cd6f01aa08
Matthew Riley <matthew.riley@barren.kyschool.us>
2023-05-01 08:16:57 -0500

    Initial Commit

---
+++ SwiftListsExercise/SwiftListsExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftListsExerciseApp.swift
+//  SwiftListsExercise
+//
+//  Created by Matthew RIley on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftListsExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
