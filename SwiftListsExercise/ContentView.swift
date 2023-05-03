ad800f3306451048fec39a2cc341a7f25ebfd17b
Matthew Riley <matthew.riley@barren.kyschool.us>
2023-05-03 08:31:59 -0500

    Created list that includes Item names and prices

--- SwiftListsExercise/ContentView.swift
+++ SwiftListsExercise/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    struct Item: Identifiable {
+        let id: UUID = UUID()
+        
+        let itemName: String
+        let price: String
+        
+    }
+    
+    let storeItems: [Item] = [
+        Item(itemName: "Soccerball", price: "5.00"),
+        Item(itemName: "Basketball", price: "6.00"),
+        Item(itemName: "Football", price: "7.00"),
+        Item(itemName: "Baseball", price: "4.00"),
+        Item(itemName: "Tennisball", price: "1.00"),
+        Item(itemName: "Volleyball", price: "4.00"),
+        Item(itemName: "Hockey Puck", price: "2.00")
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Text("Sports-R-Us")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            List(storeItems){ item in
+                
+                HStack {
+                    Text(item.itemName)
+                        .font(.title)
+                    
+                    Spacer()
+                    
+                    Text(item.price)
+                        .font(.title)
+                }
+                
+            }
+            
         }
-        .padding()
     }
 }
 
